3227f6c8f1d07fb4e563ee7d546762f7d39048a4
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-07-19 14:00:47 +0100

    Added animations

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
 
-import { ScrollContainer } from 'react-scroll-motion';
+import { ScrollContainer, ScrollPage, Animator, Sticky, ZoomIn, FadeIn, StickyIn, batch, Fade, MoveIn, MoveOut, Move } from 'react-scroll-motion';
 import './App.css';
+
+const ZoomInScrollOut = batch(StickyIn(), FadeIn(), ZoomIn());
+const FadeUp =batch(Fade(), Sticky(), Move());
 
 function App() {
   return (
     <ScrollContainer>
-      Hello world
+      <ScrollPage page={0}>
+        <Animator animation={batch(Sticky(), Fade(), MoveOut(0, -200))}>
+        <h2>Scroll animation 😊</h2>
+        </Animator>
+      </ScrollPage>
+      <ScrollPage page={1}>
+        <Animator animation={ZoomInScrollOut}>
+        <h2>Today is a great day ☀️</h2>
+        </Animator>
+      </ScrollPage>
+      <ScrollPage page={2}>
+        <Animator animation={FadeUp}>
+        <h2>Tommorow will be great 😍</h2>
+        </Animator>
+      </ScrollPage>
+      <ScrollPage page={3}>
+        <div className="section-3">
+        <h2><Animator animation={MoveIn(-1000, 0)}>Hello 🐢🐈🐳 </Animator></h2>
+        <h2><Animator animation={MoveIn(1000, 0)}>Hello You 👋 </Animator></h2>
+        <h2><Animator animation={MoveOut(1000, 0)}>Hello World 🌍 </Animator></h2>
+        <h2><Animator animation={MoveOut(-1000,0)}>You are the Star ⭐ </Animator></h2>
 
+        </div>
+      </ScrollPage>
+      <ScrollPage page={4}>
+      <Animator animation={batch(Fade(), Sticky())}>
+        <h2>The End</h2>
+      </Animator>
+      </ScrollPage>
+      
     </ScrollContainer>
   );
 }
